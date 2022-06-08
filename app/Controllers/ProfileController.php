2c319a77d82583016ab394db20a40c9f7bcacb0a
JaimeBucarey19 <bucarey19jaime@hotmail.com>
2022-06-08 13:04:28 -0400

    Commit con vista a home page

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -7,6 +7,13 @@ class ProfileController extends Controller
     public function index()
     {
         $session = session();
-        echo "Hello : ".$session->get('nombre');
+      
+
+
+        // $this->load->view('App\Views\barra.html');
+
+    //    return redirect()->to("/")->with('message','Bienvenido' .$session->username);
+    echo view('/homepage/home_page');
+    //     echo "Hello : ".$session->get('nombre');
     }
 }
