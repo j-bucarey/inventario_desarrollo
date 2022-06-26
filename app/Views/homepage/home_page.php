cc2591f80845fb48e962b8c9dab856dd12fef0d2
JaimeBucarey19 <bucarey19jaime@hotmail.com>
2022-06-25 20:59:52 -0400

    Commit de prueba jaime

--- app/Views/homepage/home_page.php
+++ app/Views/homepage/home_page.php
@@ -44,6 +44,13 @@
             </p>
           </a>
           <ul class="nav nav-treeview">
+
+          <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Registro</p>
+              </a>
+            </li>
            
             <li class="nav-item">
               <a href="#" class="nav-link">
